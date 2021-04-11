4227561250978f3a0d6518d1ec4ffbc43a2cb887
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-11 16:10:49 +0600

    Initial Commit

---
+++ UITableViewUITableCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableViewUITableCell
+//
+//  Created by Rustam Keneev on 4/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
